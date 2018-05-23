2fdd1250e9db8997b2f842c4c5bd04632c3e7715
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-05-23 16:00:12 -0300

    Mostrando as Categorias com as Imagens

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:1586"
+    baseUrl: "http://localhost:1586",
+    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic"
 }
