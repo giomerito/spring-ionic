4fcb0f9defe4db838b06b28211c7f0409ed750f3
Giomerito Alves de Souza <giomerito.souza@gmail.com>
2018-06-02 21:59:52 -0300

    Buscando enderecos do banco de dados

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { StorageService } from '../../services/storage.service';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 
 @IonicPage()
@@ -12,44 +14,29 @@ export class PickAddressPage {
 
   items: EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public storage: StorageService,
+    public clienteService: ClienteService
+  ) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua do oitiseiro",
-        numero: "1586",
-        complemento: "Casa 2",
-        bairro: "Jardim das Acácias",
-        cep: "47.850-00",
-        cidade: {
-          id: "1",
-          nome: "Luis Eduardo Magalhães",
-          estado: {
-            id: "1",
-            nome: "Bahia"
-          }
-        }
-      },
-      {
-        id: "2",
-        logradouro: "Rua Minas Gerais",
-        numero: "42",
-        complemento: "Casa 2",
-        bairro: "Mimoso I",
-        cep: "47.850-00",
-        cidade: {
-          id: "1",
-          nome: "Luis Eduardo Magalhães",
-          estado: {
-            id: "1",
-            nome: "Bahia"
-          }
-        }
-      }
-    ]
+    let localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.items = response['enderecos'];
+        },
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
+    } else {
+      this.navCtrl.setRoot('HomePage');
+    }
   }
 
 }
